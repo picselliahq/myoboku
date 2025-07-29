87f08243bbd518c155b8d171ce15bf150e750cd4
Thomas Darget <thomasdarget@hotmail.fr>
2025-07-29 17:59:57 +0200

    add port in run

--- scripts/run.py
+++ scripts/run.py
@@ -1,8 +1,12 @@
+import argparse
 import subprocess
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8000)
+    args = parser.parse_args()
     try:
         subprocess.run(["python", "app/manage.py", "migrate"])
-        subprocess.run(["python", "app/manage.py", "runserver", "8005"])
+        subprocess.run(["python", "app/manage.py", "runserver", str(args.port)])
     except KeyboardInterrupt:
         print("Shutting down this server..")
